ef93fe3fc56c0a02a5384c9d4296295a94e42fa4
SunisaManureem <sunisa2561a@gmail.com>
2025-01-12 19:40:15 +0700

    ส่งงาน

---
+++ Lab06_Exercise09/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+System.Text.StringBuilder sb = new System.Text.StringBuilder("Rat is my favorite animal.");
+       
+       System.Console.WriteLine(@$"Before change : sb = {sb.ToString()}, 
+       sb.GetHashCode = {sb.GetHashCode()}");
+       
+       System.Console.WriteLine(@"Replace first char in sb  from R to C");
+       sb[0] = 'C';
+       
+       System.Console.WriteLine(@$"After change : sb = {sb.ToString()}, 
+       sb.GetHashCode = {sb.GetHashCode()}");
